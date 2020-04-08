7a369a914824a97ecd9261eaf73ed3ecd62a3028
xakepok@xakepok.com <44duP_yx579>
2020-04-08 16:41:45 +0300

    Version 1.2

--- administrator/components/com_pp/sql/updates/mysql/1.2.sql
+++ administrator/components/com_pp/sql/updates/mysql/1.2.sql
@@ -1,9 +1,1 @@
-alter table `#__mkv_pp_operations`
-    add checked_out int unsigned not null default 0,
-    add checked_out_time datetime not null;
-
-alter table `#__mkv_pp_plan`
-    modify objectID smallint unsigned null default null;
-
-alter table `#__mkv_pp_plan` drop foreign key `#__mkv_pp_plan_#__mkv_pp_objects_objectID_id_fk`;
-
+alter table `#__mkv_pp_plan` rename to `#__mkv_pp_tasks`;
