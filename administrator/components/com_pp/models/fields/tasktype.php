7f33332a06d70f866a324eb739899480dcc836d9
xakepok@xakepok.com <44duP_yx579>
2020-04-17 20:24:59 +0300

    Filter by user groups in field

--- administrator/components/com_pp/models/fields/tasktype.php
+++ administrator/components/com_pp/models/fields/tasktype.php
@@ -10,12 +10,16 @@ class JFormFieldTasktype extends JFormFieldList
 
     protected function getOptions()
     {
+        $groups = implode(", ", JFactory::getUser()->groups);
+
         $db =& JFactory::getDbo();
         $query = $db->getQuery(true);
         $query
             ->select("t.id, t.title")
             ->from("`#__mkv_pp_task_types` t")
-            ->order("t.ordering");
+            ->order("t.ordering")
+            ->where("t.groupID in ({$groups})");
+
         $result = $db->setQuery($query)->loadObjectList();
 
         $options = array();
