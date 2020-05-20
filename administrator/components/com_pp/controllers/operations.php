5a230a140b5352acfcc942e9278120f9dbdc9676
xakepok@xakepok.com <44duP_yx579>
2020-05-20 14:21:09 +0300

    Return to referer page after operations delete

--- administrator/components/com_pp/controllers/operations.php
+++ administrator/components/com_pp/controllers/operations.php
@@ -1,5 +1,6 @@
 <?php
 use Joomla\CMS\MVC\Controller\AdminController;
+use Joomla\Utilities\ArrayHelper;
 
 defined('_JEXEC') or die;
 
@@ -8,5 +9,35 @@ class PpControllerOperations extends AdminController
     public function getModel($name = 'Operation', $prefix = 'PpModel', $config = array())
     {
         return parent::getModel($name, $prefix, $config);
+    }
+
+    public function delete()
+    {
+        $this->checkToken();
+        $cid = $this->input->get('cid', array(), 'array');
+        if (!is_array($cid) || count($cid) < 1)
+        {
+            \JLog::add(\JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), \JLog::WARNING, 'jerror');
+        }
+        else
+        {
+            $model = $this->getModel();
+            $cid = ArrayHelper::toInteger($cid);
+
+            if ($model->delete($cid))
+            {
+                $this->setMessage(\JText::plural($this->text_prefix . '_N_ITEMS_DELETED', count($cid)));
+            }
+            else
+            {
+                $this->setMessage($model->getError(), 'error');
+            }
+
+            $this->postDeleteHook($model, $cid);
+        }
+
+        $this->setRedirect($_SERVER['HTTP_REFERER']);
+        $this->redirect();
+        jexit();
     }
 }
